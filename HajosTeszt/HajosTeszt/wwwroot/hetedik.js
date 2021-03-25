015d4ba5b64354751668cc1120bcc14afb98de2b
Emese Borbély <emese.borbely@stud.uni-corvinus.hu>
2021-03-25 16:56:41 +0100

    hetedik heti feladatok, amíg meg tudtam oldani

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,18 @@
+﻿window.onload = function () {
+    function letöltés() {
+        fetch('/questions.json')
+            .then(response => response.json())
+            .then(data => letöltésBefejeződött(data)
+            );
+
+        function letöltésBefejeződött(d) {
+            console.log("Sikeres letöltés")
+            console.log(d)
+            kérdések = d;
+        }
+    }
+
+    function kérdésMegjelenítés(kérdés) {
+
+    }
+}
